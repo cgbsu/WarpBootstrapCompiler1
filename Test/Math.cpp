5d5bb1ca8ca678749974790e73dccc0765a8176e
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-08-01 21:04:15 +0000

    I decided I had enough trying to implement fixed-point. I can do it, but I dont need too, and I dont have the time.

--- Test/Math.cpp
+++ Test/Math.cpp
@@ -1,5 +1,4 @@
 #include <Warp/Utilities/Math.hpp>
-#include <Warp/Utilities/FixedPoint.hpp>
 #include <CppUTest/TestHarness.h>
 
 using namespace Warp::Utilities;
@@ -14,10 +13,5 @@ TEST(Math, ExtractDigits)
 	CHECK((extract_digits(1234, 2u, 2u) == 12));
 	CHECK((extract_digits(1234, 0u, 2u) == 34));
 	CHECK((extract_digits(1234, 1u, 2u) == 23));
-};
-
-TEST(Math, FixedPointMath)
-{
-	FixedPoint<size_t, 3u> f({3u, 5u});
 };
 
