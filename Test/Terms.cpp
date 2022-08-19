3df0696e2dd9fe5096ab2a29cd8f89d505a26505
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-08-19 06:52:00 +0000

    Added another case.

--- Test/Terms.cpp
+++ Test/Terms.cpp
@@ -551,6 +551,34 @@ TEST(Terms, MergeTermsUnevenInterleavingCase)
 					::AddOnePriority<QUANTERNARY_TEST_TERMS>
 					::AddOnePriority<SENERY_TEST_TERMS>
 		>);
+	static_assert(std::is_same_v<
+			MergeTerms<
+					Terms<
+							Terms<
+									Terms<
+											Terms<TermsNoPreviousType, 0, TEST_TERMS>, 
+											3, 
+											SECONDARY_TEST_TERMS
+										>, 
+									4, 
+									QUINARY_TEST_TERMS
+								>, 
+							5, 
+							SENERY_TEST_TERMS
+						>, 
+					Terms<
+							Terms<TermsNoPreviousType, 1, TERCIARY_TEST_TERMS>, 
+							2, 
+							QUANTERNARY_TEST_TERMS
+						>
+				>, 
+			MakeTerms<TEST_TERMS>
+					::AddOnePriority<TERCIARY_TEST_TERMS>
+					::AddOnePriority<QUANTERNARY_TEST_TERMS>
+					::AddOnePriority<SECONDARY_TEST_TERMS>
+					::AddOnePriority<QUINARY_TEST_TERMS>
+					::AddOnePriority<SENERY_TEST_TERMS>
+		>);
 	// </Uneven Interleaving> //
 };
 
