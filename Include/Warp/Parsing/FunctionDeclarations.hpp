fbcf57f749a7f371ea5e03759c2b50a74da99827
cgbsu@github.com <cgeeley@student.bridgew.edu>
2022-09-02 05:46:36 +0000

    Checkpoint

--- Include/Warp/Parsing/FunctionDeclarations.hpp
+++ Include/Warp/Parsing/FunctionDeclarations.hpp
@@ -93,102 +93,98 @@ namespace Warp::Parsing
 					>
 			>;
 
-		using ThisTermsType = BaseTermsType;//MergeTerms<BaseTermsType, UniqueTermsType>;
+		using TermsType = MergeTerms<BaseTermsType, UniqueTermsType>;
 
 
 		using WholeMathematicalParserType = HomogenousMathematicalExpressionParser<
 				NumericTypeTag::Whole, 
-				ThisTermsType, 
+				MathematicalExpressionTermsType, 
 				TypeResolverParameterTemplate
 			>;
 
 		//using IntegerMathematicalParserType = HomogenousMathematicalExpressionParser<
 		//		NumericTypeTag::Integer, 
-		//		ThisTermsType, 
+		//		MathematicalExpressionTermsType, 
 		//		TypeResolverParameterTemplate
 		//	>;
 
 		//using FixedPointMathematicalParserType = HomogenousMathematicalExpressionParser<
 		//		NumericTypeTag::FixedPoint, 
-		//		ThisTermsType, 
+		//		MathematicalExpressionTermsType, 
 		//		TypeResolverParameterTemplate
 		//	>;
 
 		//using CharacterMathematicalParserType = HomogenousMathematicalExpressionParser<
 		//		NumericTypeTag::Character, 
-		//		ThisTermsType, 
+		//		MathematicalExpressionTermsType, 
 		//		TypeResolverParameterTemplate
 		//	>;
 
 		//using BoolMathematicalParserType = HomogenousMathematicalExpressionParser<
 		//		NumericTypeTag::Bool, 
-		//		ThisTermsType, 
+		//		MathematicalExpressionTermsType, 
 		//		TypeResolverParameterTemplate
 		//	>;
 
-		using TermsType = ThisTermsType;
-		//using TermsType //= MergeTerms<
-				//MergeTerms<BaseTermsType, UniqueTermsType>, 
-				//typename WholeMathematicalParserType::UniqueTermsType;
-			//>;
+		//using TermsType = ThisTermsType;
 		//typename WholeMathematicalParserType::UniqueTermsType>;
 
 		template<auto TermTagParameterConstant>
 		constexpr static const auto term = TermsType::template term<TermTagParameterConstant>;
 
-		//constexpr static const auto let_keyword = term<Keyword::Let>;
-		//constexpr static const auto equal = term<MultiPurposeOperator::Equal>;
-		//constexpr static const auto identifier = term<Identifier::Identifier>;
-		//constexpr static const auto open_parenthesis = term<Brackets::OpenParenthesis>;
-		//constexpr static const auto close_parenthesis = term<Brackets::CloseParenthesis>;
-		//constexpr static const auto semi_colon = term<Declaration::SemiColon>;
-		//constexpr static const auto constant_declaration = term<Declaration::Constant>;
-		//constexpr static const auto constant = term<Construct::Constant>;
+		constexpr static const auto let_keyword = term<Keyword::Let>;
+		constexpr static const auto equal = term<MultiPurposeOperator::Equal>;
+		constexpr static const auto identifier = term<Identifier::Identifier>;
+		constexpr static const auto open_parenthesis = term<Brackets::OpenParenthesis>;
+		constexpr static const auto close_parenthesis = term<Brackets::CloseParenthesis>;
+		constexpr static const auto semi_colon = term<Declaration::SemiColon>;
+		constexpr static const auto constant_declaration = term<Declaration::Constant>;
+		constexpr static const auto constant = term<Construct::Constant>;
 
 		//constexpr static const auto reduce_to = term<reduce_to_tag>;
 
 		constexpr static const auto whole_terms = WholeMathematicalParserType::terms;
 
-		constexpr static const auto terms = whole_terms;
-		//constexpr static const auto terms = concatinate_tuples(
+		//constexpr static const auto terms //= whole_terms;
+		constexpr static const auto terms = concatinate_tuples(
 		//		whole_terms, 
-		//		//WholeMathematicalParserType::terms, 
+				WholeMathematicalParserType::terms, 
 		//		//WholeMathematicalParserType::unique_terms, 
 		//		//IntegerMathematicalParserType::unique_terms, 
 		//		//FixedPointMathematicalParserType::unique_terms, 
 		//		//CharacterMathematicalParserType::unique_terms, 
 		//		//BoolMathematicalParserType::unique_terms, 
-		//		ctpg::terms(
-		//				let_keyword, 
-		//				equal, 
-		//				identifier, 
-		//				open_parenthesis, 
-		//				close_parenthesis, 
-		//				semi_colon
-		//			)
-		//	);
+				ctpg::terms(
+						let_keyword, 
+						equal, 
+						identifier, 
+						open_parenthesis, 
+						close_parenthesis, 
+						semi_colon
+					)
+			);
 
-		constexpr static const auto whole_non_terminal_terms = WholeMathematicalParserType::non_terminal_terms;
-		constexpr static const auto non_terminal_terms = whole_non_terminal_terms;
-		//constexpr static const auto non_terminal_terms = concatinate_tuples(
+		//constexpr static const auto whole_non_terminal_terms = WholeMathematicalParserType::non_terminal_terms;
+		//constexpr static const auto non_terminal_terms = whole_non_terminal_terms;
+		constexpr static const auto non_terminal_terms = concatinate_tuples(
 		//		whole_non_terminal_terms, 
-		//		//WholeMathematicalParserType::non_terminal_terms, 
+				WholeMathematicalParserType::non_terminal_terms, 
 		//		//WholeMathematicalParserType::unique_non_terminal_terms, 
 		//		//IntegerMathematicalParserType::unique_non_terminal_terms, 
 		//		//FixedPointMathematicalParserType::unique_non_terminal_terms, 
 		//		//CharacterMathematicalParserType::unique_non_terminal_terms, 
 		//		//BoolMathematicalParserType::unique_non_terminal_terms, 
-		//		ctpg::terms(
-		//				constant_declaration, 
-		//				constant
-		//			)
-		//	);
+				ctpg::terms(
+						constant_declaration, 
+						constant
+					)
+			);
 
-		//constexpr static const auto constant_declaration_rule 
-		//		= constant_declaration(let_keyword, identifier, equal)
-		//		>= [](auto let, auto name, auto equal) {
-		//			return std::string{name};
-		//		};
+		constexpr static const auto constant_declaration_rule 
+				= constant_declaration(let_keyword, identifier, equal)
+				>= [](auto let, auto name, auto equal) {
+					return std::string{name};
+				};
 
 		//template<typename MathematicalExpressionGeneratorParameterType>
 		//constexpr static const auto constant_from_math_term()
@@ -219,23 +215,23 @@ namespace Warp::Parsing
 		//		);
 		//}
 
-		//consteval static const auto unique_rules()
-		//{
-		//	return //concatinate_tuples(
-		//			//constant_from_math_term<WholeMathematicalParserType>(), 
-		//			ctpg::rules(
-		//					constant_declaration_rule
-		//				)
-		//		//);
-		//	;
-		//}
+		consteval static const auto unique_rules()
+		{
+			return //concatinate_tuples(
+					//constant_from_math_term<WholeMathematicalParserType>(), 
+					ctpg::rules(
+							constant_declaration_rule
+						)
+				//);
+			;
+		}
 
 		consteval static const auto rules()
 		{
-			return //concatinate_tuples( 
-					WholeMathematicalParserType::BaseType::rules();//,
-					//unique_rules()
-				//);
+			return concatinate_tuples( 
+					WholeMathematicalParserType::BaseType::rules(),
+					unique_rules()
+				);
 		}
 					
 	};
